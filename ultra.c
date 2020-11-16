da319ee8116cbd47af08ab02c54dd96e14f8a653
AriM2270 <73492845+AriM2270@users.noreply.github.com>
2020-11-16 13:06:03 -0700

    Update ultra.c

--- ultra.c
+++ ultra.c
@@ -1,3 +1,5 @@
+//code created by 
+
 uint32_t i;
 uint32_t error = 0;
 
@@ -41,11 +43,6 @@ void uart2(void){
         EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // Put eUSCI in reset
         EUSCI_A0->CTLW0 = EUSCI_A_CTLW0_SWRST | // Remain eUSCI in reset
                 EUSCI_B_CTLW0_SSEL__SMCLK;      // Configure eUSCI clock source for SMCLK
-        // Baud Rate calculation
-        // 12000000/(16*9600) = 78.125
-        // Fractional portion = 0.125
-        // User's Guide Table 21-4: UCBRSx = 0x10
-        // UCBRFx = int ( (78.125-78)*16) = 2
         EUSCI_A0->BRW = 78;                     // 12000000/16/9600
         EUSCI_A0->MCTLW = (2 << EUSCI_A_MCTLW_BRF_OFS) |
                 EUSCI_A_MCTLW_OS16;
@@ -63,11 +60,9 @@ void timer(void){
 
         __enable_irq();             // Enables interrupts to the system
 
-        NVIC->ISER[1] = 1 << ((PORT2_IRQn) & 31);       // Very important to assign interrupts to the NVIC vector otherwise they would not
-                                                        // considered
-        NVIC->ISER[0] = 1 << ((TA0_0_IRQn) & 31);
-        //    __low_power_mode_3();
-        //    __no_operation();
+        //NVIC->ISER[1] = 1 << ((PORT2_IRQn) & 31);       
+                                                       
+        //NVIC->ISER[0] = 1 << ((TA0_0_IRQn) & 31);
 }
 
 int uart_puts(const char *str)
