80fc9c31ca68e7c48b31e00b3be95ae51512be86
AriM2270 <73492845+AriM2270@users.noreply.github.com>
2020-11-18 11:53:39 -0700

    Update ultra.c

--- ultra.c
+++ ultra.c
@@ -17,9 +17,9 @@ void uart(void){
 
 void gpio(void){
     // Configure GPIO
-        P1->DIR |= BIT0;                        // Set P1.0 as output
+        P1->DIR |= BIT0;                        // Set P1.0 as output for onboard LED 
         P1->OUT |= BIT0;                        // P1.0 high
-        P1->OUT &= ~BIT0;
+        P1->OUT &= ~BIT0;                       // P1.0 low
 }
 
 void uart2(void){
@@ -27,9 +27,9 @@ void uart2(void){
             P1->SEL0 |= BIT2 | BIT3;                // set 2-UART pin as secondary function
 
 
-        P2->DIR &= ~BIT7;            // P2.4 as o
-        P2->REN |= BIT7;            // P2.4 pull down enabled
-        P2->OUT &= ~BIT7;            // P2.4 initial output Low
+        P2->DIR &= ~BIT7;            // P2.7 as input
+        P2->REN |= BIT7;            // P2.7 pull down enabled
+        P2->OUT &= ~BIT7;            // P2.7 initial output 0, as changing it to 1 triggers LED 
 
         P2->SEL0 = 0;
         P2->SEL1 = 0;
@@ -41,32 +41,27 @@ void uart2(void){
 
 
         // Configure UART
-        EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // Put eUSCI in reset
-        EUSCI_A0->CTLW0 = EUSCI_A_CTLW0_SWRST | // Remain eUSCI in reset
-                EUSCI_B_CTLW0_SSEL__SMCLK;      // Configure eUSCI clock source for SMCLK
-        EUSCI_A0->BRW = 78;                     // 12000000/16/9600
+        EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // Put eUSCI (UART ports) in reset mode 
+        EUSCI_A0->CTLW0 = EUSCI_A_CTLW0_SWRST | // Force eUSCI to remain in reset
+                EUSCI_B_CTLW0_SSEL__SMCLK;      // Configure eUSCI clock source
+        EUSCI_A0->BRW = 78;                     
         EUSCI_A0->MCTLW = (2 << EUSCI_A_MCTLW_BRF_OFS) |
                 EUSCI_A_MCTLW_OS16;
 
         EUSCI_A0->CTLW0 &= ~EUSCI_A_CTLW0_SWRST; // Initialize eUSCI
-        EUSCI_A0->IFG &= ~EUSCI_A_IFG_RXIFG;    // Clear eUSCI RX interrupt flag
-        EUSCI_A0->IE &= ~EUSCI_A_IE_RXIE;        // Disable USCI_A0 RX interrupt
+        EUSCI_A0->IFG &= ~EUSCI_A_IFG_RXIFG;    // Clear eUSCI interrupt flag for RX
+        EUSCI_A0->IE &= ~EUSCI_A_IE_RXIE;        // Disable interrupt for RX
 }
 
 void timer(void){
-    /*              TIMER A0            */
-        TIMER_A0->CCTL[0]= TIMER_A_CCTLN_CCIE;       // CCR0 interrupt enabled
-        TIMER_A0->CCR[0] = 1000-1;                   // 1ms at 1mhz
-        TIMER_A0->CTL = TIMER_A_CTL_TASSEL_2 | TIMER_A_CTL_MC__UP | TIMER_A_CTL_CLR;                  // SMCLK, upmode
+        TIMER_A0->CCTL[0]= TIMER_A_CCTLN_CCIE;       //Enable TimerA0 interrupt 
+        TIMER_A0->CCR[0] = 1000-1;                   
+        TIMER_A0->CTL = TIMER_A_CTL_TASSEL_2 | TIMER_A_CTL_MC__UP | TIMER_A_CTL_CLR;                  //set TimerA0 to upmode
 
-        __enable_irq();             // Enables interrupts to the system
-
-        //NVIC->ISER[1] = 1 << ((PORT2_IRQn) & 31);       
-                                                       
-        //NVIC->ISER[0] = 1 << ((TA0_0_IRQn) & 31);
+        __enable_irq();             // Enables interrupts
 }
 
-int uart_puts(const char *str)
+int uart_puts(const char *str) //transmit UART data to the main file 
 {
     int status = -1;
 
@@ -74,13 +69,10 @@ int uart_puts(const char *str)
         status = 0;
 
         while (*str != '\0') {
-            /* Wait for the transmit buffer to be ready */
             while (!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
 
-            /* Transmit data */
-            EUSCI_A0->TXBUF  = *str;
-
-            /* If there is a line-feed, add a carriage return */
+            EUSCI_A0->TXBUF  = *str; //transmit UART data 
+            
             if (*str == '\n') {
                 /* Wait for the transmit buffer to be ready */
                 while (!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
@@ -91,5 +83,5 @@ int uart_puts(const char *str)
         }
     }
 
-    return status;
+    return status; //returning raw ultrasonic data 
 }
